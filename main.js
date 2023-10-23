09f97b4659a6b3f0c10511e3d99f330f1e178be1
Marina Perin <maryfrancisco@live.com>
2023-10-23 15:05:56 +0200

    feat | temperature converter exercise done

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 let coffeePrice = 1.50;
 let coffeeQuantity = prompt('How many cups of coffee were sold?');
 let coffeeQuantityNumber = parseInt(coffeeQuantity);
-alert(coffeePrice * coffeeQuantityNumber);
+alert('Today income was € ' + coffeePrice * coffeeQuantityNumber);
 
 
 /* THE AGE CALCULATOR */
@@ -11,11 +11,13 @@ alert(coffeePrice * coffeeQuantityNumber);
 let currentYear = 2023;
 let userYear = prompt('In which year were you born?');
 let userYearNumber = parseInt(userYear);
-alert(currentYear - userYearNumber);
+alert('You are ' + (currentYear - userYearNumber) + ' years old');
 
 /* THE TEMPERATURE CONVERTER */
 
-
+let fahrenheitTemperature = prompt('How many °F is it today?');
+let fahrenheitTemperatureNumber = parseInt(fahrenheitTemperature);
+alert('Today there is °C ' + (fahrenheitTemperatureNumber - 32) * 5 / 9);
 
 /* THE GRADE CALCULATOR */
 
