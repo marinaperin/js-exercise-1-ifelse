ca70a7d0d9c414f15cbb5b34555c4827a7024f9c
Marina Perin <maryfrancisco@live.com>
2023-10-23 14:45:07 +0200

    feat | coffee shop exercise done

---
+++ main.js
@@ -0,0 +1,25 @@
+/* THE COFFEE SHOP */ 
+
+let coffeePrice = 1.50;
+let coffeeQuantity = prompt('How many cups of coffee were sold?');
+let coffeeQuantityNumber = parseInt(coffeeQuantity);
+alert(coffeePrice * coffeeQuantityNumber);
+
+
+/* THE AGE CALCULATOR */
+
+/* THE TEMPERATURE CONVERTER */
+
+/* THE GRADE CALCULATOR */
+
+/* THE TIP CALCULATOR */
+
+/* THE MOVIE NIGHT */
+
+/* THE DISCOUNT CALCULATOR */
+
+/* THE BMI CALCULATOR */
+
+/* THE LEAP YEAR CHECKER */
+
+/* THE GRADE ASSIGNER */
